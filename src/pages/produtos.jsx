e7b44172de4e64c0d89bc017f29362073ecea6f4
Aluno IFMA <alunos.aca@ifma.edu>
2024-08-21 17:52:24 -0300

    akaçk

--- src/pages/produtos.jsx
+++ src/pages/produtos.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState} from 'react'
 import '../App.css'
 import {fireStoreDB} from "../services/firebaseConfig.js"
 import { addDoc, collection, getDocs, getFirestore, doc , deleteDoc} from 'firebase/firestore'
+import { useNavigate} from "react-router-dom/dist"
+import { Link } from "react-router-dom/dist"
 
 
 
@@ -11,6 +13,8 @@ export default function Produtos() {
     const [name, setName] = useState("")
     const [valor, setValor] = useState(null)
     const [cursos, setCursos] = useState([])
+
+    const navigate = useNavigate()
 
     //const db = getFirestore(firebase)
     const useCollectionRef = collection(fireStoreDB, "Cursos")
@@ -39,6 +43,7 @@ export default function Produtos() {
                     valor,
                 })
                 console.log(user)
+                return navigate("/home")
             }}>criar produto</button>
 
             <ul>
@@ -47,7 +52,10 @@ export default function Produtos() {
                         <div key={curso.id} className='produtosItem'>
                             <li><p>Nome do Curso: {curso.name}</p></li>
                             <li>Valor do Curso: {curso.valor}</li>
-                            <button onClick={() => deletarCurso(curso.id)}>Deletar</button>
+                            <button onClick={() => {
+                                deletarCurso(curso.id)
+                                return navigate("/home")
+                                }}>Deletar</button>
                         </div>
                     )
                 })}
